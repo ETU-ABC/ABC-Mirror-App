647b3581398b5cb609d1b0a434c39501d4fa51af
Cemal Kılıç <cemalkilic96@gmail.com>
2019-03-27 23:06:47 +0300

    Weather :: Update weather by city name, autocomplete for city names

--- src/screens/WeatherScreen.js
+++ src/screens/WeatherScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, Button, TextInput, Text, View, ScrollView} from 'react-native';
 import axios from 'axios';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 export default class WeatherScreen extends React.Component {
   static navigationOptions = {
@@ -13,22 +14,10 @@ export default class WeatherScreen extends React.Component {
     this.url =  this.host + '/edit?module=currentweather';
   }
 
-  // the most horrible way to do this
-  cityDict = {
-      'Ankara': 323786,
-      'Istanbul': 745044,
-      'İstanbul': 745044,
-      'Malatya': 304919,
-      'Antalya':323776 ,
-      'Bursa': 750268,
-      'Eskişehir': 315201,
-      'Eskisehir': 315201
-  };
-
   sendRequest = () => {
     axios.post( this.url, {
         "content": {
-            "locationID": this.cityDict[this.state.text]
+            "location": this.state.text
         }
     })
     .then(function (response) {
@@ -45,31 +34,83 @@ export default class WeatherScreen extends React.Component {
       <View style={styles.container}>
         <ScrollView>
         <Text style={styles.welcome}>
-            Enter the city name for weather
+            City name for weather
         </Text>
-        <TextInput
-          style={styles.schoolNo}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        <Button
-          style={styles.module_button}
-          onPress={this.sendRequest}
-          title="Update"
-          color = "#bfffc4"
-          accessibilityLabel="Update weather"
-        />
+
         <Text style={styles.info}>
-          Enter the city name for updating weather info in the mirror
+          Start typing the city name, select the city you want and press 'Update' button to update weather in the mirror.
         </Text>
+
+            <GooglePlacesAutocomplete
+                placeholder='Search'
+                minLength={2} // minimum length of text to search
+                autoFocus={false}
+                returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+                keyboardAppearance={'light'} // Can be left out for default keyboardAppearance https://facebook.github.io/react-native/docs/textinput.html#keyboardappearance
+                listViewDisplayed='auto'    // true/false/undefined
+                fetchDetails={true}
+                renderDescription={row => row.description} // custom description render
+                onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                    // check details.formatted_address is null?
+                    const text = details.formatted_address;
+                    this.setState({text});
+                }}
+
+                getDefaultValue={() => ''}
+
+                query={{
+                    // available options: https://developers.google.com/places/web-service/autocomplete
+                    // !!! type in key !!!
+                    // key: '<super_secret_key>',
+                    language: 'en', // language of the results
+                    types: '(cities)' // default: 'geocode'
+                }}
+
+                styles={{
+                    textInputContainer: {
+                        width: '100%'
+                    },
+                    description: {
+                        fontWeight: 'bold'
+                    },
+                    predefinedPlacesDescription: {
+                        color: '#1faadb'
+                    }
+                }}
+
+                currentLocation={false} // Will add a 'Current location' button at the top of the predefined places list
+                nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+                GoogleReverseGeocodingQuery={{
+                    // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+                }}
+                GooglePlacesSearchQuery={{
+                    // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                    rankby: 'distance'
+                }}
+
+                GooglePlacesDetailsQuery={{
+                    // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
+                    fields: 'formatted_address',
+                }}
+
+                // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+                filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']}
+                debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+            />
+
+            <Button
+                style={styles.module_button}
+                onPress={this.sendRequest}
+                title="Update"
+                color = "#bfffc4"
+                accessibilityLabel="Update weather"
+            />
 
         </ScrollView>
       </View>
     );
   }
 }
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
